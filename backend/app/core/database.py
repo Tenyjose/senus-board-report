ef7f17cf727666c953cce66e9a3cd406b174eff6
tenyjose18 <tenyjose18@gmail.com>
2026-07-05 09:51:29 +0100

    feat: add configuration and database connection setup

---
+++ backend/app/core/database.py
@@ -0,0 +1,23 @@
+# Database connection setup.
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from app.core.config import settings
+
+
+# connection to the db
+engine = create_engine(settings.database_url)
+
+#for individual sessions
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
